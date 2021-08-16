2138add1c737c1f804b23b9c4bb447637ae233c8
Fjuro <fjuro@seznam.cz>
2021-08-16 16:13:31 +0200

    Add files via upload

--- generate.php
+++ generate.php
@@ -1,36 +1,64 @@
 <?php
 if( !empty( $_POST ) ){
-	header('Content-disposition: attachment; filename=manifest.json');
+    header('Content-disposition: attachment; filename=manifest.json');
     header('Content-type: application/json');
     $file = 'fest.json';
     $arr = array(
         "server_name" => $_POST["server_name"],
         "nice_name" => $_POST["nice_name"],
         "direct_ip" => $_POST["direct_ip"],
-        "server_wildcards" => array("placeholder"),
-        "social" => array(
-            "web" => $_POST["web"],
-            "web_shop" => $_POST['web_shop'],
-            "web_support" => $_POST["web_support"],
-            "twitter" => $_POST["twitter"],
-            "tiktok" => $_POST["tiktok"],
-            "facebook" => $_POST["facebook"],
-            "instagram" => $_POST["instagram"],
-            "discord" => $_POST["discord"],
-            "youtube" => $_POST["youtube"],
-            "teamspeak" => $_POST["teamspeak"]
-        ),
-        "discord" => array(
-            "server_id" => $_POST["server_id"],
-        ),
-        "brand" => array(
-            "primary" => $_POST["primary"],
-            "background" => $_POST["background"],
-            "text" => $_POST["text"]
-        ),
-        "yt_trailer" => $_POST["yt_trailer"],
-        "user_stats" => $_POST["user_stats"]
+    if (isset($_POST['server_wildcards']) && $_POST['server_wildcards'] !== '') {
+        $arr['server_wildcards'] = explode(',', $_POST['server_wildcards']);
+    }
     );
+    if (isset($_POST['web']) && $_POST['web'] !== '') {
+        $arr['social']['web'] = $_POST['web'];
+    }
+    if (isset($_POST['web_shop']) && $_POST['web_shop'] !== '') {
+        $arr['social']['web_shop'] = $_POST['web_shop'];
+    }
+    if (isset($_POST['web_support']) && $_POST['web_support'] !== '') {
+        $arr['social']['web_support'] = $_POST['web_support'];
+    }
+    if (isset($_POST['twitter']) && $_POST['twitter'] !== '') {
+        $arr['social']['twitter'] = $_POST['twitter'];
+    }
+    if (isset($_POST['tiktok']) && $_POST['tiktok'] !== '') {
+        $arr['social']['tiktok'] = $_POST['tiktok'];
+    }
+    if (isset($_POST['facebook']) && $_POST['facebook'] !== '') {
+        $arr['social']['facebook'] = $_POST['facebook'];
+    }
+    if (isset($_POST['instagram']) && $_POST['instagram'] !== '') {
+        $arr['social']['instagram'] = $_POST['instagram'];
+    }
+    if (isset($_POST['discord']) && $_POST['discord'] !== '') {
+        $arr['social']['discord'] = $_POST['discord'];
+    }
+    if (isset($_POST['youtube']) && $_POST['youtube'] !== '') {
+        $arr['social']['youtube'] = $_POST['youtube'];
+    }
+    if (isset($_POST['teamspeak']) && $_POST['teamspeak'] !== '') {
+        $arr['social']['teamspeak'] = $_POST['teamspeak'];
+    }
+    if (isset($_POST['server_id']) && $_POST['server_id'] !== '') {
+        $arr['discord']['server_id'] = $_POST['server_id'];
+    }
+    if (isset($_POST['primary']) && $_POST['primary'] !== '') {
+        $arr['brand']['primary'] = $_POST['primary'];
+    }
+    if (isset($_POST['background']) && $_POST['background'] !== '') {
+        $arr['brand']['background'] = $_POST['background'];
+    }
+    if (isset($_POST['text']) && $_POST['text'] !== '') {
+        $arr['brand']['text'] = $_POST['text'];
+    }
+    if (isset($_POST['yt_trailer']) && $_POST['yt_trailer'] !== '') {
+        $arr['yt_trailer'] = $_POST['yt_trailer'];
+    }
+    if (isset($_POST['user_stats']) && $_POST['user_stats'] !== '') {
+        $arr['user_stats'] = $_POST['user_stats'];
+    }
     $json_string = json_encode($arr, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
     file_put_contents($file, $json_string);
     echo $json_string;
--- index.php
+++ index.php
@@ -3,22 +3,28 @@
 <head>
     <title>LabyMod server media generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/b528589131.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <div style="text-align: center; width: 30%; margin: auto;">
   <h1>Form</h1>
     <form name="form" method="post" enctype="multipart/form-data" action="generate.php" autocomplete="off">
         <div class="mb-3">
-            <label class="form-label" for="server_name">Server name: </label>
+            <label class="form-label" for="server_name"><code>*</code>Server name: </label>
             <input class="form-control" type="text" name="server_name" id="server_name" placeholder="laby" required>
         </div>
         <div class="mb-3">
-            <label class="form-label" for="nice_name">Nice name: </label>
+            <label class="form-label" for="nice_name"><code>*</code>Nice name: </label>
             <input class="form-control" type="text" name="nice_name" id="nice_name" placeholder="LabyMod" required>
         </div>
         <div class="mb-3">
-            <label class="form-label" for="direct_ip">Direct IP: </label>
+            <label class="form-label" for="direct_ip"><code>*</code>Direct IP: </label>
             <input class="form-control" type="text" name="direct_ip" id="direct_ip" placeholder="play.laby.net" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="server_wildcards">Server wildcards: </label>
+            <input class="form-control" type="text" name="server_wildcards" id="server_wildcards" placeholder="%.laby.net, %.labymod.net" aria-describedby="wildcards_help">
+            <div id="wildcards_help" class="form-text">Separate the wildcards with commas.</div>
         </div>
       <h4>Social</h4>
         <div class="mb-3">
@@ -89,9 +95,35 @@
             <input class="form-control" type="text" name="user_stats" id="user_stats" placeholder="https://laby.net/@{userName}">
         </div>
         <p style="text-align: center;">
-            <input class="btn btn-primary" type="submit" name="submit" id="submit" value="Submit">
+            <input class="btn btn-primary btn-lg" type="submit" name="submit" id="submit" value="Submit">
         </div>
     </form>
 </div>
+<footer class="pt-5 my-5 text-muted border-top" style="margin: 30px">
+    &copy; <?php echo date("Y"); ?> Fjuro &middot; <a href="#credits" data-bs-toggle="modal">Credits</a>
+</footer>
+<!-- Modal -->
+<div class="modal fade" id="credits" tabindex="-1" aria-labelledby="creditsTitle" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="creditsTitle">Credits</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <a href="https://www.w3schools.com">W3Schools</a> for amazing HTML and PHP tutorials<br>
+        <a href="https://laby.net/@Niklas">Niklas</a> for helping me with code<br>
+        <a href="https://getbootstrap.com/">Bootstrap</a> for awesome CSS framework<br>
+        Many people from <a href="https://stackoverflow.com">Stack Overflow</a><br>
+        LabyMedia for making <a href="https://labymod.net">LabyMod</a>, the best Minecraft modification
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <a href="https://github.com/Fjuro/LabyGenerator" class="button btn btn-primary"><i class="fab fa-github"></i> GitHub</a>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 </html>
